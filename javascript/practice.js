01f7add1a853f870f54eb4e3fe408d445676d893
Adity Banik Arpa <aditybanikarpa@gmail.com>
2026-09-02 13:29:34 +0600

    Add practice.js with basic variable and arithmetic operations

---
+++ javascript/practice.js
@@ -0,0 +1,18 @@
+let name="Rahim";
+let age=20;
+let department="CSE";
+let university="Premier University";
+
+console.log("Name:"+name);
+console.log("Age:"+age);
+console.log("Department:"+department);
+console.log("University:"+university);
+
+let a=20;
+let b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
